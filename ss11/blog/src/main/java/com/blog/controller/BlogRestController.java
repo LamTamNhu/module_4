924adf3df787d7cf628aed3fa552d41eba336283
LamTamNhu <46390185+LamTamNhu@users.noreply.github.com>
2024-01-04 00:15:00 +0700

    *HUH

--- ss11/blog/src/main/java/com/blog/controller/BlogRestController.java
+++ ss11/blog/src/main/java/com/blog/controller/BlogRestController.java
@@ -7,17 +7,19 @@ import com.blog.service.IBlogHasCategoryService;
 import com.blog.service.IBlogService;
 import com.blog.service.ICategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
+@RequestMapping("/api")
 public class BlogRestController {
     @Autowired
     ICategoryService categoryService;
@@ -33,8 +35,8 @@ public class BlogRestController {
     }
 
     @GetMapping("/blogs")
-    public ResponseEntity<Iterable<Blog>> getAllBlogs() {
-        Iterable<Blog> blogs = blogService.findAll();
+    public ResponseEntity<Iterable<Blog>> getAllBlogs(@PageableDefault(value = 3) Pageable pageable) {
+        Page<Blog> blogs = blogService.getAll(pageable);
         if (blogs == null) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
